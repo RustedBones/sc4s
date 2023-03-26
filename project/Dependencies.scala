2e4283a13f1af776f1c73f99f41e2a1686da88d2
Scala Steward <scala_steward@virtuslab.com>
2023-03-26 17:47:45 +0000

    Update annotations to 16.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val Annotations  = "16.0.2"
+    val Annotations  = "16.0.3"
     val BouncyCastle = "1.69"
     val Circe        = "0.14.3"
     val Gson         = "2.9.0"
